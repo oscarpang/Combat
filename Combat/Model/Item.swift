610be29c8f6fefc266cc2669a9f2efcc21d905b4
QINGWEI LAN <qingweilan@gmail.com>
2015-07-03 17:46:46 +0800

    Added ItemsViewController with basic structure

---
+++ Combat/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Combat
+//
+//  Created by Qingwei Lan on 7/3/15.
+//  Copyright (c) 2015 Qingwei Lan. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject
+{
+
+}
